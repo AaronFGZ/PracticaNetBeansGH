0001071f53064970e7c0c751fb80a2e6b011eacb
A15AaronFG <A15AaronFG@a21eq29.sanclemente.local>
2016-05-09 19:47:17 +0200

    "Clase Alumno Añadida"

---
+++ PracticaNetBeansGH/src/practicanetbeansgh/Alumno.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practicanetbeansgh;
+
+/**
+ *
+ * @author A15AaronFG
+ */
+public class Alumno {
+    
+}
